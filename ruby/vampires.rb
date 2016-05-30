3f5f5475387328147b7b42825a8f0ad9d128d59e
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-05-30 17:03:00 -0400

    Add number of employees processed

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,54 +1,59 @@
-puts "What is your name?"
-	name = gets.chomp
+puts "How many employees would you like processed?"
+	n = gets.to_i
+n.times do
 
-puts "Hello #{name} it is very nice to meet you...Vampire cough cough"
-
-puts "How old are you?"
-	age = gets.to_i
-puts "What year were you born?"
-	birth_year = gets.to_i
-puts "Could you remind me what year it is?"
-	current_year = gets.to_i
+	puts "What is your name?"
+		name = gets.chomp
 	
-	if age + birth_year == current_year
-		puts "Probably not a vampire"
-	else
-		puts "Probably a vampire"
-	end
-
-prompt = "> "
-puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n?"
-print prompt
+	puts "Hello #{name} it is very nice to meet you...Vampire cough cough"
 	
-while garlic = gets.chomp
-	case garlic
-	when "y"
-		puts "Probably not a vampire"
-		break
-	when "n"
-		puts "Probably a vampire"
-		break
-	else
-		puts "Please answer y or n"
-		print prompt
+	puts "How old are you?"
+		age = gets.to_i
+	puts "What year were you born?"
+		birth_year = gets.to_i
+	puts "Could you remind me what year it is?"
+		current_year = gets.to_i
+		
+		if age + birth_year == current_year
+			puts "Probably not a vampire"
+		else
+			puts "Probably a vampire"
+		end
+	
+	prompt = "> "
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n?"
+	print prompt
+		
+	while garlic = gets.chomp
+		case garlic
+		when "y"
+			puts "Probably not a vampire"
+			break
+		when "n"
+			puts "Probably a vampire"
+			break
+		else
+			puts "Please answer y or n"
+			print prompt
+		end
 	end
-end
-
-
-prompt = "> "
-puts "Would you like to enroll in the company’s health insurance? y/n?"
-print prompt
-
-while insurance = gets.chomp
-	case insurance
-	when "y"
-		puts "Probably not a vampire"
-		break
-	when "n"
-		puts "Probably a vampire"
-		break
-	else
-		puts "Please answer y or n"
-		print prompt
+	
+	prompt = "> "
+	puts "Would you like to enroll in the company’s health insurance? y/n?"
+	print prompt
+	
+	while insurance = gets.chomp
+		case insurance
+		when "y"
+			puts "Probably not a vampire"
+			break
+		when "n"
+			puts "Probably a vampire"
+			break
+		else
+			puts "Please answer y or n"
+			print prompt
+		end
 	end
+	
 end
