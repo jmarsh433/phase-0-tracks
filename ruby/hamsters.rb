c1d3fd9831c9c14db6bb64ca80ceb70cbff72007
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-05-29 20:32:25 -0400

    Add changes to hamsters.rb from peer pair's repo

---
+++ ruby/hamsters.rb
@@ -0,0 +1,44 @@
+# NAME -- STRING
+puts "What is the hamster's name?"
+name = gets.chomp
+
+# VOLUME LEVEL -- INT
+puts "How light of a sleeper are you? (1-10)"
+volume = gets.to_i
+
+# FUR COLOR -- COLOR
+puts "What color is the hamster's fur?"
+fur_color = gets.chomp
+
+# GOOD CANDIDATE -- BOOL
+puts "Is the hamster a good candidate? (Y/N)"
+good_candidate = gets.chomp
+# good_candidate = "".to_b
+Y = true
+N = false 
+
+until good_candidate == Y
+  if good_candidate == Y
+    good_candidate = true
+  else good_candidate == N
+    good_candidate = false
+    puts "Are you sure?"
+    answer = gets.chomp
+    good_candidate = true
+  end
+end
+
+
+# ESTIMATED AGE -- INT
+puts "How old is the hamster?"
+est_age = gets.to_i
+
+if est_age == ""
+  est_age = nil
+end
+
+
+hamster_data = [name, volume, fur_color, good_candidate, est_age]
+puts "#{hamster_data}"
+
+puts "The hampster's name is #{name}, owner's volume level: #{volume}, color of fur is #{fur_color}, is it a good candidate #{good_candidate}, it's age is #{est_age}"
