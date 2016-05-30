7ab11b0aece1cfd127c48aa85a832f1ae053b891
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-05-30 15:52:11 -0400

    Add first condition for mathematical age of suspect

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+2016 - age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
+
