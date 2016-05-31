ed2c8add23755612aaa7da18291a7a1e104ca91b
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-05-31 00:33:31 -0400

    Add cryptography application

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,21 +9,46 @@ def encrypt(word)
     end
     i += 1
   end
-  return word
+  word
 end 
 
-
-
 #Decryption Method
-def decrypt(string)
+def decrypt(word)
     i = 0
     correct_string = ""
-  while i < string.length
+  while i < word.length
     alph = "abcdefghijklmnopqrstuvwxyz"
-      letter_position = alph.index(string[i])
+      letter_position = alph.index(word[i])
     string_value = alph[letter_position - 1]
       correct_string += string_value  
     i += 1
   end
-  return correct_string
+  correct_string
 end
+
+#decrypt(encrypt("tiger"))
+#this works because I believe the notation between my two methods is standardized
+#setting an index per letter of the string decrypted/encrypted keeps the same notation between the two methods allowing the encrypt to nest inside the decrypt method.
+
+#Cryptography Application
+def intelligence
+  puts "Would you like to encrypt or decrypt your message?"
+    cryption = gets.chomp
+  if cryption == "encrypt"
+    puts "type message here"
+    encrypt(gets.chomp)
+  elsif cryption == "decrypt"
+    puts "type message here"
+    decrypt(gets.chomp)
+  else
+    puts "This program will detonate in"
+    puts 5
+    puts 4
+    puts 3
+    puts 2
+    puts 1
+    puts "Goodbye :)"
+  end
+end
+
+
