1efbdf45c393bd72cf7e2f83f6dbd63076a33496
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-03 17:58:41 -0400

    Add release 1 to release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,25 @@
+# # RELEASE 1
+actors_array = ["Brad Pitt", "Marky Mark", "Leo", "Matt Damon", "George Clooney"]
+
+
+role_hash = {
+    actors_array[0] => "Tyler Durden",
+    actors_array[1] => "Boogie Nights",
+    actors_array[2] => "Wolf of Wallstreet",
+    actors_array[3] => "Bourne",
+    actors_array[4] => "Oceans 11"
+}
+role_hash.each do |actor, role|
+    puts "#{actor}, #{role}"
+end
+
+actors_array.map! do |actor|
+    puts "#{actor}"
+    actors_array.shuffle!(random: Random.new)
+end
+
+
+#RELEASE 2
 
 #Array Delete
 array_a = [1, 2, 3, 4, 5]
@@ -17,7 +39,13 @@ p array_a
 
 #Array Step 3
 
+array_c = [100, 4, 18, 56, 30]
 
+array_c.each do |x|
+	next if x <= 50
+	array_c.delete(x)
+	end
+p array_c
 
 #Array Step 4
 array_b = ["red", "yellow", "blue", "orange", "white"]
@@ -35,14 +63,14 @@ array_b.each do |x|
 end
 
 
-
+#Hash Filter
 hash_a = {
 	1 => "one",
 	2 => "two",
 	3 => "three",
 	4 => "four"
 	}
-#Hash Filter
+
 hash_a.each do |x, int|
 	next if x.even? == true
 		p "#{int}"
@@ -56,6 +84,19 @@ hash_a.each do |x, int|
 p hash_a
 
 #Hash Step 3
+hash_b = {
+	bill: 22,
+	ted: 45,
+	susan: 18,
+	josh: 3
+	}
+
+hash_b.each do |name, age|
+	next if name == :bill
+		hash_b.delete(:bill)
+	p hash_b
+end
+p hash_b
 
 #Hash Step 4
 x = true
