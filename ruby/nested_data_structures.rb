586cea24ae42ad7291d5376a5543cb744e25095a
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-06 02:51:21 -0400

    Add nested array inside hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,9 @@
+#Nested Structures
+#Housing Renovation Worker Location
+
+marchand_renovation = {
+	'basement' => ['framing', 'carpeting', 'plumbing'] 
+	'bathroom' => ['tiling', 'sink', 'bathtub']
+	'bedroom' => ['master_bed', 'curtains', 'wallpaper']
+	'kitchen' => ['table', 'chairs', 'wood_flooring']
+	}
