09c2c20d8a3da538ea986167fd1f6d57e19e56d6
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-06 17:01:36 -0400

    Combine methods into one method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,33 +1,77 @@
-#Just change vowels to next vowel
+#PROGRAM FOR NEXT VOWEL
 
-def vowel_changer(word)
+def next_vowel(word)
 	i = 0
 	correct_string = ""
 	while i < word.length
-		vowel = "aeiou"
-			vowel_position = vowel.index(word[i])
+	vowel = "aeioua"
+		if vowel_position = vowel.index(word[i])
 			string_value = vowel[vowel_position +1]
 			correct_string += string_value
-			i += 1
+		else
+			word[i]	
+		end
+	i += 1
 	end
 	correct_string
 end
 
-vowel_changer("aeio")
 
-#Just change the consonants to next consonant
+#PROGRAM FOR NEXT CONSONANT
 
-def consonant_changer(word)
+def next_conso(word)
 	i = 0
 	correct_string = ""
 	while i < word.length
-		consonant = "bcdfghjklmnpqrstvwxyz"
-			consonant_position = consonant.index(word[i])
+	consonant = "bcdfghjklmnpqrstvwxyzb"
+		if consonant_position = consonant.index(word[i])
 			string_value = consonant[consonant_position +1]
 			correct_string += string_value
-			i += 1
+		else 
+			word[i]
+		end
+		i += 1
 	end
 	correct_string
 end
 
-consonant_changer("fghj")
+
+
+#COMBINING METHODS
+
+def new_alias(word)
+	i = 0
+	correct_string = ""
+	while i < word.length
+	vowel = "aeioua"
+	consonant = "bcdfghjklmnpqrstvwxyzb"
+		if consonant_position = consonant.index(word[i])
+			string_value = consonant[consonant_position +1]
+			correct_string += string_value
+		elsif vowel_position = vowel.index(word[i])
+			string_value = vowel[vowel_position +1]
+			correct_string += string_value
+		elsif " "
+			" "
+		end
+		i += 1
+	end
+	correct_string
+end
+
+new_alias("marc handzu")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
