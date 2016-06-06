07a5bf488b67fd723831c8fe2a42dd156a78d853
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-06 04:14:20 -0400

    Fix format for hash and array.  Add puts to call specific values.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,21 +3,51 @@
 
 marchand_renovation = {
 	basement: {
-		'unfinished_work': ['framing', 'carpeting', 'plumbing'],
-		'completeed_word': ['wiring', 'foundation']
-	}
+		unfinished_work: [
+						'framing', 
+						'carpeting', 
+						'plumbing'
+						],
+		completeed_word: [
+					'wiring', 
+					'foundation'
+					]
+	},
 	bathroom: { 
-		'unfinished_work': ['tiling', 'sink', 'bathtub'],
-		'finished_work': ['wiring', 'painting']
-	}
+		unfinished_work: [
+						'tiling', 
+						'sink', 
+						'bathtub'
+						],
+		finished_work: [
+					'wiring', 
+					'painting'
+					]
+	},
 	bedroom: {
-		'unfinished_work': ['master_bed', 'curtains', 'wallpaper'],
-		'finished_work': ['carpeting', 'wiring', 'lighting']
-	}
+		unfinished_work: [
+						'master_bed', 
+						'curtains', 
+						'wallpaper'
+						],
+		finished_work: [
+						'carpeting', 
+						'wiring', 
+						'lighting'
+						]
+	},
 	kitchen: {
-		'unfinished_work': ['painting', 'wiring'],
-		'finished_work': ['stove', 'counter']
+		unfinished_work: [
+						'painting', 
+						'wiring'
+						],
+		finished_work: [
+						'stove', 
+						'counter'
+						]
 	}
 }
 
-marchand_renovation[:basement]
+puts marchand_renovation[:basement][:unfinished_work][2]
+puts marchand_renovation[:kitchen][:unfinished_work]
+puts marchand_renovation[:bedroom][:finished_work][1]
