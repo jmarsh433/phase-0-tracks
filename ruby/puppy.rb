d42e75c8255a7a4ec0f291a257d2d2ee33ca9584
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-09 16:02:13 -0400

    Add puppy class characteristics/behavior to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# SPECIES ------
+# Waggalus Lickapotomus
+
+# CHARACTERISTICS ------
+# Tail Length: short
+# Ear Type: floppy
+# Fur Length: short
+# Bark Pitch: high
+
+# BEHAVIOR ------
+# Run
+# Romp
+# Stomp
