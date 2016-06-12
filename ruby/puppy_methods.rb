009796e2f147ceeb63bfc137be3d14f66a798b8e
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-12 19:25:51 -0400

    Add release 2 information

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,35 +1,39 @@
 class Puppy
 
-  def initialize
-  	puts "Initializing new puppy instance ..."
-  end	
+  def initialize(toy, barks, human_years)
+    puts "Initializing new puppy instance ..."
+    @toy = toy
+    @barks = barks
+    @human_years = human_years
+  end  
 
   def fetch(toy)
     @toy = toy
     puts "I brought back the #{toy}!"
   end
 
-  def speak(n)
-  n.times do
-  		puts "Woof!"
-  	end
+  def speak(barks)
+  barks.times do
+      puts "Woof!"
+    end
   end
 
   def roll_over
-  	puts "rolls over"
+    puts "rolls over"
   end
 
   def play_dead
-  	puts "plays dead"
+    puts "plays dead"
   end
 
   def dog_years(human_years)
-  	puts human_years * 7
+    puts human_years * 7
   end
 
 end
 
-puppy = Puppy.new
+puppy = Puppy.new("ball", 2, 3)
+# puppy.new("ball", 2)
 puppy.fetch("ball")
 puppy.speak(7)
 puppy.roll_over
@@ -38,29 +42,55 @@ puppy.dog_years(7)
 
 #Release 2
 class Golf
+  def initialize(club)
+    puts "Initializing golf..."
+    @club = club
+    @example_clubs = ["driver", "5-iron", "sand-wedge", "putter", "neel", "jon", "max"]
+  end
 
-	def initialize
-		puts "Initializing golf..."
-	end
+  def swing(club_opt="")
+    # @club = club
+    puts "Swing #{@club}"
+  end
 
-	def swing(club)
-		@club = club
-		puts "Swing #{club}"
-	end
+  def chip(club_opt="")
+    # @club = club
+    puts "Chips with #{@club}"
+  end
 
-	def chip(club)
-		@club = club
-		puts "Chips with #{club}"
-	end
-
-	def put
-		puts "Lines up put"
-	end
+  def putt
+    puts "Lines up putt"
+  end
+  
+  def randomize_club(example_clubs)
+    50.times do |new_club|
+    @random_club = @example_clubs.shuffle.first
+    return @random_club
+    end
+  end
+  
 end
 
-golf = Golf.new
-golf.swing("7 iron")
+
+
+random_clubs_array = []
+
+
+
+golf = Golf.new("9-iron")
+golf.randomize_club(@example_clubs)
+golf.swing("7 iron") 
 golf.chip("Sand wedge")
-golf.put
+golf.putt
+golf.swing("6 iron")
+golf.chip("Pitching wedge")
+golf.putt
+
+golf.randomize_club(@example_clubs)
+random_clubs_array << @random_club
+
+p random_clubs_array
 
 
+golf.each do |new_club|
+  golf.randomize_club(example_clubs)  
