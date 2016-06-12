8605412101667f3a81a50477e5a320a9b3eb85e9
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-12 13:13:14 -0400

    Fix times method for puppy bark

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,12 +1,32 @@
 class Puppy
 
-  def initialize(toy)
+  def initialize
+  	puts "Initializing new puppy instance ..."
+  end	
 
+  def fetch(toy)
     @toy = toy
     puts "I brought back the #{toy}!"
-    
+  end
+
+  def woof(n)
+  n.times do
+  		puts "Woof!"
+  	end
+  end
+
+  def roll_over
+  	puts "rolls over"
+  end
+
+  def dog_years(human_years)
+  	puts human_years * 7
   end
 
 end
 
-Puppy.new("ball")
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.woof(7)
+puppy.roll_over
+puppy.dog_years(7)
--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,15 +16,15 @@ class Santa
 	end
 end
 
-# santa = Santa.new("male", "white")
-# santa.speak
-# santa.eat_milk_and_cookies("Snickerdoodle")
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+santa = Santa.new("male", "white")
+santa.speak
+santa.eat_milk_and_cookies("Snickerdoodle")
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
