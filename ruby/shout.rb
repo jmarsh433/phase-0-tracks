8239847649eb566c0779f8b1778164f08a96dbbf
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-16 14:38:23 -0400

    Convert a standalone to a mixin ...release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,44 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#   	words + "!!!" + " O_o"
+#   end
+# end
+
+# puts Shout.yell_angrily("get at me")
+# puts Shout.yelling_happily("Im so cool")
+
+
+#Release 3
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yelling_happily(words)
-  	words + "!!!" + " O_o"
-  end
+	def die_hard(bruce_quote)
+		p bruce_quote + ":0"
+	end
+
+	def few_good_men(catch_phrase)
+		p catch_phrase + ":x!!!!"
+	end
 end
 
-puts Shout.yell_angrily("get at me")
-puts Shout.yelling_happily("Im so cool")
+class Bruce_Willis
+	include Shout
+end
+
+class Jack_Nicholson
+	include Shout
+end
 
 
-#Driver Code
+bald_dude = Bruce_Willis.new
+bald_dude.die_hard("YIPPY-KAY-YAE F***A!")
+bald_dude.few_good_men("Happy Trails Hans!!!")
+
+
+
+old_dude = Jack_Nicholson.new
+old_dude.few_good_men("YOU CAN'T HANDLE THE TRUTH!")
+old_dude.die_hard("Here's Johnny!")
 
