b6593b4c4eab75640569e3b3a2add6a95de68333
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-17 15:38:53 -0400

    Final touches on release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -40,61 +40,16 @@ puppy.roll_over
 puppy.play_dead
 puppy.dog_years(7)
 
-#Release 2
-class Golf
-  def initialize(club)
-    puts "Initializing golf..."
-    @club = club
-    @example_clubs = ["driver", "5-iron", "sand-wedge", "putter", "8-iron"]
-  end
-
-  def swing(swing)
-    @club = club
-    puts "Swing #{@club}"
-  end
-
-  def chip(chip)
-    @club = club
-    puts "Chips with #{@club}"
-  end
-
-  def putt
-    puts "Lines up putt"
-  end 
-end
-
-  # def randomize_club(example_clubs)
-  #   50.times do |new_club|
-  #   @random_club = @example_clubs.shuffle.first
-  #   return @random_club
-  #   end
-  # end
-
-# random_clubs_array = []
-
-
-
-golf = Golf.new("9-iron")
-golf.randomize_club(@example_clubs)
-golf.swing("7 iron") 
-golf.chip("Sand wedge")
-golf.putt
-
-# golf.randomize_club(@example_clubs)
-# random_clubs_array << @random_club
-
-# p random_clubs_array
-
-
-# golf.each do |new_club|
-#   golf.randomize_club(example_clubs)  
-
-
 #Hockey release 2
   class Hockey
+    attr_reader :players, :shots, :dangles
+    attr_accessor :dangle, :shot, :save
     def initialize(dangle, shot)
       puts "Initializing hockey instance..."
-      @player = "Bobby Orr"
+      @players = ["Gretzsky", "Orr", "Crosby", "Ovie", "Lundqvist", "Kessel", "Elias"]
+      @shots = ["wristshot", "snapshot", "slapshot", "knucklepuck"]
+      @dangles = ["Michigan", "toe-drag", "between-the-legs", "triple-deke"]
+      @randomized_hockey_array = []
       @dangle = dangle
       @shot = shot
     end
@@ -106,18 +61,54 @@ golf.putt
     def shot(power)
       puts "#{@player} fires a #{@shot} on goal!"
     end
+
+    def save()
+    end
+
+    def randomized_combination
+      rand_p = @players.shuffle.first
+      rand_s = @shots.shuffle.first
+      rand_d = @dangles.shuffle.first
+      @randomized_hockey_array << rand_p
+      @randomized_hockey_array << rand_s
+      @randomized_hockey_array << rand_d
+      p @randomized_hockey_array
+    end
+
+    # def random_player
+    #   @randomized_hockey_array << @players.shuffle.first
+    # end
+
+    # def random_shot
+    #   @randomized_hockey_array << @shots.shuffle.first
+    # end
+
+    # def random_dangle
+    #   @randomized_hockey_array << @dangles.shuffle.first
+    # end
   end
-
 puck = Hockey.new("toe drag", "slapshot")
-puck.dangle("toe drag") 
-puck.shot("slapshot")
+puck.players
+p puck.players
+# puck.dangle("toe drag") 
+# puck.shot("slapshot")
+# puck = []
+# @shots = ["wristshot", "snapshot", "slapshot", "knucklepuck"]
+# @dangles = ["Michigan", "toe-drag", "between-the-legs", "triple-deke"]
+# @example_shots.length.times do |i|
+#   puck << Hockey.new(@shots[i], @dangles[i])
+# end
 
-puck = []
-@shots = ["wristshot", "snapshot", "slapshot", "knucklepuck"]
-@dangles = ["Michigan", "toe-drag", "between-the-legs", "triple-deke"]
+# puck.random_player
+# puck.random_dangle
+# puck.random_shot
+combo = puck.randomized_combination
+puts combo
+i = 0
+until i == 50
+  puts "#{puck.randomized_combination}\n\n"
 
-@example_shots.length.times do |i|
-  puck << Hockey.new(@shots[i], @dangles[i])
+  i += 1
 end
 
 
