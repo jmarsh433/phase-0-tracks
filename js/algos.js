7c6a9a211189e0ecd401211af7f3410da00431a3
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-19 21:05:34 -0400

    sort by length and return first value

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// I want to pull the longest item from an array.
+// Options: Somehow find function to pull the longest string length
+// Organize the array by string length (utilize a sort function)
+// pull the first item (item 0)
+
+
+
+function longest(array) {
+	array.sort(bylength);
+	return array[0];
+}
