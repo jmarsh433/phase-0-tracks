89ad4123a8a7bba82300cf6d07f3608157d2cf47
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-20 03:47:01 -0400

    Add pseudo code for release 1, and final code with driver code

--- js/algos.js
+++ js/algos.js
@@ -1,39 +1,10 @@
+// Release 0 - Find the Longest Phrase
+
+// PSEUDOCODE
 // I want to pull the longest item from an array.
-// Options: Somehow find function to pull the longest string length
-// Organize the array by string length (utilize a sort function)
+// Sort the items from longest to shortest by string length
+// set this organized list according to length longest to shortest as a new array
 // pull the first item (item 0)
-
-// function longest(array) {
-// 	array.sort(bylength);
-// 	return array[0];
-// }
-
-// // found problems with this 
-// // through research i found this sort function and applied it to my array
-
-// var randArray = ['happy', 'sad', 'California', 'Fender']
-
-// rand_array.sort(function(a,b){
-// 	return a.length - b.length;
-// });
-
-// // Now I want to return the longest phrase in the array
-
-// rand_array.sort(function(a,b){
-// 	new
-// 	return a.length - b.length;
-// });
-
-// var rand_array = ['happy', 'sad', 'California', 'Fender']
-
-// rand_array.sort(function(a,b){
-// 	var firstItem = "";
-// 	firstItem += b.length - a.length;
-// 	return firstItem[0];
-// });
-
-// for some reason this code does not return the first item of the new array.
-// I decided to call the item outside the function
 
 var rand_array = ['happy', 'sad', 'California', 'Fender']
 
@@ -43,4 +14,33 @@ rand_array.sort(function(a,b){
 
 console.log(rand_array[0])
 
+
+// Release 1 Find a Key value match
+
+// PSEUDOCODE
+// need a way to link the keys from the two objects into an if else statement
+// set condition that if values from keys are == then return true
+// need a condition that if just one of the keys contains equal values even if the other doesnt, returns true
+// else they are not equal and should return false
+
+vvar o1 = {name: "Ted", age: 54};
+var o2 = {name: "Bill", age: 53};
+var o3 = {name: "Harry", age: 27}
+var o4 = {name: "Ted", age: 53}
+
+
+
+function keyValueMatch(o1, o2) {
+	if (o1.age == o2.age || o1.name == o2.name) {
+			console.log("true");
+	}	else {
+			console.log("false");
+	}
+}
+
+// Driver Code
+keyValueMatch(o2, o4)
+keyValueMatch(o1, o2)
+keyValueMatch(o3, o4)
+keyValueMatch(o1, o3)
 
