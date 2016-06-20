78bad7f74ddea65d963d516dea4b5858fb349714
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-19 21:07:20 -0400

    Add function sort to organize shortest string to longest

--- js/algos.js
+++ js/algos.js
@@ -3,9 +3,16 @@
 // Organize the array by string length (utilize a sort function)
 // pull the first item (item 0)
 
-
-
 function longest(array) {
 	array.sort(bylength);
 	return array[0];
 }
+
+// found problems with this 
+// through research i found this sort function and applied it to my array
+
+var rand_array = ['happy', 'sad', 'California', 'Fender']
+
+rand_array.sort(function(a,b){
+	return a.length - b.length;
+});
