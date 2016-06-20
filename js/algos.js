05cf9d61fe6d57669027450c665cf24f061e4da8
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-19 22:03:32 -0400

    Add function to return longest item

--- js/algos.js
+++ js/algos.js
@@ -3,16 +3,32 @@
 // Organize the array by string length (utilize a sort function)
 // pull the first item (item 0)
 
-function longest(array) {
-	array.sort(bylength);
-	return array[0];
-}
+// function longest(array) {
+// 	array.sort(bylength);
+// 	return array[0];
+// }
 
-// found problems with this 
-// through research i found this sort function and applied it to my array
+// // found problems with this 
+// // through research i found this sort function and applied it to my array
+
+// var randArray = ['happy', 'sad', 'California', 'Fender']
+
+// rand_array.sort(function(a,b){
+// 	return a.length - b.length;
+// });
+
+// // Now I want to return the longest phrase in the array
+
+// rand_array.sort(function(a,b){
+// 	new
+// 	return a.length - b.length;
+// });
 
 var rand_array = ['happy', 'sad', 'California', 'Fender']
 
 rand_array.sort(function(a,b){
-	return a.length - b.length;
+	var firstItem = ""
+	firstItem += b.length - a.length;
+	return firstItem[0];
 });
+
