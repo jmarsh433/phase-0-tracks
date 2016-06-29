1b2dcbb9a5a4c86c8090859fa9f505ce805d058d
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-06-29 14:05:14 -0400

    Add pseudocode for penetentiary

---
+++ sql_ruby/marchand_penetentiary.rb
@@ -0,0 +1,9 @@
+#Take in prisoner name
+#Record prisoner crime
+#Record prisoner sentence
+#Assign a random prisoner number
+#ASsign a prisoner random cell
+	#The cells will be displayed by cell block
+	#Should assign guard on duty at the cell block
+#Write user interface for Warden so he can access inmate information
+
