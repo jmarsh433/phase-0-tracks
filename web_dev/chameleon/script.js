d288873bf73818f4a895a6a634a1e6e398833de9
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-07-01 10:25:57 -0400

    Release 1 and 2 9.2 challenge

--- web_dev/chameleon/script.js
+++ web_dev/chameleon/script.js
@@ -1,10 +1,28 @@
 console.log("The script is running!");
 
+// Release 1
+var list = document.getElementsByTagName("ul");
+	list[0].style.border="2px solid blue";
+
+
 function addPinkBorder(event) {
   console.log("click happened! here's the click event:");
   console.log(event);
   event.target.style.border = "2px solid pink";
 }
 
+// Release 2
+
+function changeBackgroundColor(event) {
+	event.target.style.backgroundColor = "green";
+	console.log("event");
+}
+
+
 var photo = document.getElementById("lizard-photo");
 photo.addEventListener("click", addPinkBorder);
+
+var background = document;
+background.addEventListener("click", changeBackgroundColor);
+
+
