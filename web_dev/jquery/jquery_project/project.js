92bd4729c0f19067bda7c3e76c3468cb9c427a23
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-07-10 22:36:29 -0400

    Add jquery project

--- web_dev/jquery/jquery_project/project.js
+++ web_dev/jquery/jquery_project/project.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
+	// BASIC SELECTORS
+	$('h1').css({ 'color': 'blue', 'font-size': '150%', 'font-family': 'Georgia'});
+	$('p').css('border', '2px solid red');
+
+	// BASIC ANIMATIONS
+	$('h1').slideUp(500).delay(300).show(800);
+
+	// INDEX FILTERS
+	$('p:eq(2)').css('border', '4px solid green');
 
 });
+
