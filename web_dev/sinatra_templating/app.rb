f1cfc60122b013e51901b492da74fab7cf962961
Jonathan M. Marchand <jon.marchand1@gmail.com>
2016-07-10 20:39:32 -0400

    Add releases 0, 1, 2 challenge 9.5

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,46 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/find' do
+	erb :find_student
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/search' do
+  p params
+  name = params[:name]
+  id = params[:id]
+  age = params[:age]
+  campus = params[:campus]
+  response = ""
+  sql_statment = "SELECT * FROM students "
+
+    sql_statment << "WHERE "
+    if name != ""
+      sql_statment << "name='#{name}' AND "
+    end
+    if id != ""
+      sql_statment << "id='#{id}' AND "
+    end
+    if age != ""
+      sql_statment << "age='#{age}' AND "
+    end
+    if campus != ""
+      sql_statment << "campus='#{campus}' AND "
+    end
+    sql_statment << "ID>'0'"
+
+  @students = db.execute(sql_statment)
+  erb :home
+end
+
+
+
 
 # add static resources
